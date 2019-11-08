433c6aa01237c38dd9f002c0a6753d7bb99a3b8c
jaythomasv29 <jaythomasv29@gmail.com>
2019-11-08 15:00:19 -0500

    fixed bug in result component

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -10,17 +10,34 @@ export class ResultComponent implements OnInit {
   Name: string;
   category: Number;
   score
+  isLoggedIn: boolean
 
   constructor(private router: Router) {
     this.Name = history.state.data.name
     this.category = history.state.data.category
+    this.isLoggedIn =  history.state.data.isLoggedIn
     this.score = history.state.data.score
     console.log("HISTORY STATE:", history.state.data)
     console.log(this.score)
+    
   }
 
   ngOnInit() {
 
+  }
+
+  newQuiz() {
+    this.router.navigate(['/select'],
+      {
+        state: {
+          data: {
+            name: this.Name,
+            isLoggedIn: this.isLoggedIn
+          }
+
+        }
+      });
+    // console.log(this.Name)
   }
 
 }
